2f224e307cb7932f48d40350c4cc2f8b3f4055cf
Hrudayeswar <phruday5@gmail.com>
2025-12-24 11:03:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,4 +36,11 @@ for i in range(x):
     for j in range(i+1):
         print("*",end=" ")
     print()    
+
+#number pattern
+x=int(input())
+for i in range(x):
+    for j in range(i+1):
+        print(j,end=" ")
+    print()    
         
