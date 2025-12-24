254d35d5febbe0dd2f88a2147abe89ca92e45708
Hrudayeswar <phruday5@gmail.com>
2025-12-24 10:17:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+#version1
 x=int(input())
 for i in range(1,x+1):
     if(i%3==0) and (i%5==0):
         print("fizz buzz")
+    elif(i%5==0):
+        print("buzz")
+    elif(i%3==0):
+        print("fizz")
+    else:
+        print(i)
+
+#version2
+x=int(input())
+for i in range(1,x+1):
+    if(i%3==0):
+        if(i%5==0):
+            print("fizz buzz")
     elif(i%5==0):
         print("buzz")
     elif(i%3==0):
