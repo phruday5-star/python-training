8f0433a76d21238a20f2c00ba8f5b155461a3bea
Hrudayeswar <phruday5@gmail.com>
2025-12-24 11:59:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,4 +43,38 @@ for i in range(x):
     for j in range(i+1):
         print(j,end=" ")
     print()    
+
+
+#pyramid pattern 
+x=int(input())
+for i in range(x):
+    for j in range(x-i-1):
+        print(" ",end="")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()    
+
+#diamond pattern
+x=int(input())
+for i in range(x):
+    for j in range(x-i-1):
+        print(" ",end="")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+for i in range(x-2,-1,-1):
+    for j in range(x-i-1):
+        print(" ",end="")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()   
+    
+#odd pattern
+x=int(input())
+for i in range(x):
+    for j in range(x-i-1):
+        print(" ",end="")
+    for j in range(2*i+1):
+        print("*",end="")
+    print()
         
