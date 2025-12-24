25fb15baad953cd61979f718e79862583cef39e4
Hrudayeswar <phruday5@gmail.com>
2025-12-24 12:21:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -77,4 +77,14 @@ for i in range(x):
     for j in range(2*i+1):
         print("*",end=" ")
     print()
+
+#hallow square
+n=int(input())
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==0 or i==n-1 or j==n-1:
+           print("*",end=" ")
+        else:
+             print(" ",end=" ")
+    print()
         
