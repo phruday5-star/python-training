fb2d9d9bfa3f5655b478e99ea34e4ef79138d4ab
Hrudayeswar <phruday5@gmail.com>
2025-12-24 12:00:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -73,8 +73,8 @@ for i in range(x-2,-1,-1):
 x=int(input())
 for i in range(x):
     for j in range(x-i-1):
-        print(" ",end="")
+        print(" ",end=" ")
     for j in range(2*i+1):
-        print("*",end="")
+        print("*",end=" ")
     print()
         
