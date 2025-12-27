33dfeb553224d484ed0d6c830d9aabf28f92d6bd
Hrudayeswar <phruday5@gmail.com>
2025-12-27 09:52:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -160,3 +160,13 @@ for i in range (n):
         else: 
             print(" ",end=" ")
     print()
+
+#min and max values
+x=list(map(int,input().split()))
+min1=max1=x[0]
+for i in range(1,len(x)):
+    if x[i]<min1:
+       min1=x[i]
+    if x[i]>max1:
+        max1=x[i]
+print(min1,max1)
