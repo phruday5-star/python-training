332eaeff064990446804b3771ab059ca125fc3fa
Hrudayeswar <phruday5@gmail.com>
2025-12-27 11:44:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -175,6 +175,20 @@ print(min1,max1)
 x=[1,2,3,4,5]
 l=0
 r=len(x)-1
+
+#problem 283 in leetcode
+def movezeroes(nums):
+    x=[]
+    y=[]
+    for i in range(len(nums)):
+        if nums[i]==0:
+            x.append(nums[i])
+        else:
+            y.append(nums[i])
+    z=y+x
+    print(z)
+nums=[0,1,0,3,12]
+movezeroes(nums)
 while l<r:
     x[l],x[r]=x[r],x[l]
     l+=1
