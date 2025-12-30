de7f65f4829720494e6dd3340c59330344ff9dee
Hrudayeswar <phruday5@gmail.com>
2025-12-30 15:44:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -241,6 +241,23 @@ fib(n)
 def linear_search(arr,target):
     for i in range(len(arr)):
         if arr[i]==target:
+
+            #Binary_search
+def binary_search(arr,target):
+    left=0
+    right=len(arr)-1
+    while left<=right:
+        mid=(left+right)//2
+        if arr[mid]==target:
+            return mid
+        elif arr[mid]<target:
+            left=mid+1
+        else:
+            right=mid-1
+    return-1
+arr=[6,7,8,55]
+target=55
+print(binary_search(arr,target))
             return i
     return-1
 arr=list(map(int,input().split(",")))
