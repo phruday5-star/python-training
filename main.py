4db2b614a43f3397964e5d692262e37365986262
Hrudayeswar <phruday5@gmail.com>
2025-12-30 15:36:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -235,3 +235,14 @@ def fib(n):
         print()
 n=int(input())
 fib(n)
+
+#liear serch
+
+def linear_search(arr,target):
+    for i in range(len(arr)):
+        if arr[i]==target:
+            return i
+    return-1
+arr=list(map(int,input().split(",")))
+target=int(input())
+print(linear_search(arr,target))
