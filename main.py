281952a69d018a29bc91f32b69b24a702d5a291d
Hrudayeswar <phruday5@gmail.com>
2025-12-30 15:55:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -276,3 +276,15 @@ arr=list(map(int,input().split(",")))
 bubble_sort(arr)
 print("sorted array:",arr)
     
+#insertion_sort
+def insertion_sort(arr):
+    for i in range(1,len(arr)):
+        start=arr[1]
+        j=i-1
+        while j>=0 and start<arr[j]:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=start
+arr=[2,6,3,1,4]
+insertion_sort(arr)
+print("sorted array:",arr)
