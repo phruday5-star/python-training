fe7dce45ed41a5023286c32b6cc77c5b2950ddbd
Hrudayeswar <phruday5@gmail.com>
2025-12-30 16:01:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -288,3 +288,17 @@ def insertion_sort(arr):
 arr=[2,6,3,1,4]
 insertion_sort(arr)
 print("sorted array:",arr)
+
+
+#selection_sort
+def selection_sort(arr):
+    n=len(arr)
+    for i in range(n):
+        lower=i
+        for j in range(i+1,n):
+            if arr[j]<arr[lower]:
+                lower=j
+            arr[i],arr[lower]=arr[lower],arr[i]
+arr=[8,4,48,26,3]
+selection_sort(arr)
+print("sorted array:",arr)
